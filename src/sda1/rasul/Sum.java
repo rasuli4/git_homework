4e2196b37823b4723ef1d44ded9be65b79a58fdf
Rasul Turabov <rasuli4@gmail.com>
2018-10-09 17:58:59 +0300

    This part of application sums the entered numbers the desired number of times.

--- src/sda1/rasul/Sum.java
+++ src/sda1/rasul/Sum.java
@@ -7,13 +7,23 @@ public class Sum {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter 5 numbers below:");
+        System.out.println("Enter the number of elements to scan below:");
+
+        int amountOfElementsToScan = scanner.nextInt();                     // scan the number of digits
+
+        if (amountOfElementsToScan <= 0) {                                  // in case number is less or equals zero, then exit the program
+            System.out.println("Error! Number is '0' or less!");
+            return;
+        }
 
         int sum = 0;
 
-        for (int i = 0; i < 5; i++) {
+        System.out.println("Enter the elements you want to sum:");
+
+        for (int i = 0; i < amountOfElementsToScan; i++) {                  // add the scanned element to the existing sum
             sum += scanner.nextInt();
         }
-        System.out.println("Sum equals " + sum);
+        System.out.println("Sum equals: " + sum);                            // print the sum
     }
 }
+
